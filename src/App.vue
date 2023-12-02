7bc30a57da11e9ecd5c0ca214d48a1016c9c9e63
Medha Gitay <medhagitay@gmail.com>
2023-12-02 18:00:25 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>Ninja Reaction timer ! </h1>
+  <button @click="start" :disabled="isPLaying">Play</button>
+  <Block v-if="isPLaying" :delay="delay" @end="endGame"></Block>
+  <results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/results.vue'
+export default {
+  name: 'App',
+  components: {Block, Results},
+  data(){
+    return {
+      isPLaying:false,
+      delay:null,
+      score:null,
+      showResults:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay=2000+Math.random()*5000,
+      this.isPLaying=true
+      this.showResults=false
+    },
+    endGame(reactionTime){
+        this.score = reactionTime
+        this.isPLaying = false
+        this.showResults=true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
